35b4145ed37d5f4bee4e2f17d696d6252b192bbf
Gaurav <gpdhande12@gmail.com>
2024-06-25 13:56:37 +0530

    adding contacts to addressbook

--- Com/AddresBook/Uc/AddressBookMain.java
+++ Com/AddresBook/Uc/AddressBookMain.java
@@ -1,5 +1,8 @@
 package Com.AddresBook.Uc;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookMain 
 {
 	private String firstName;
@@ -7,14 +10,14 @@ public class AddressBookMain
     private String address;
     private String city;
     private String state;
-    private int zipCode;
-    private long phoneNumber;
+    private String zipCode;
+    private String phoneNumber;
     private String email;
     
     
 
-	AddressBookMain(String firstName, String lastName, String address, String city, String state, int zipCode,
-			long phoneNumber, String email) 
+	AddressBookMain(String firstName, String lastName, String address, String city, String state, String zipCode,
+			String phoneNumber, String email) 
 	{
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -39,8 +42,34 @@ public class AddressBookMain
 	public static void main(String[] args) 
 	{
 		System.out.println(" Welcome to Address Book");
-		AddressBookMain addressBook = new AddressBookMain("Gaurav","Dhande","Jay ashtvinayk bldg","Mumbai","Maharashtra",421202,977139767,"gpdhande12@gmail.com");
-		addressBook.PrintAddresBookDetails();
+		 System.out.print("How many contacts do you want to enter?: ");
+	        Scanner scanner = new Scanner(System.in);
+	        int num = scanner.nextInt();
+	        scanner.nextLine();
+	        ArrayList<AddressBookMain> contacts = new ArrayList<AddressBookMain>();
+	        for(int i = 0; i < num; i++){
+	            System.out.print("Enter the first name: ");
+	            String firstName = scanner.nextLine();
+	            System.out.print("Enter the last name: ");
+	            String lastName = scanner.nextLine();
+	            System.out.print("Enter the address: ");
+	            String address = scanner.nextLine();
+	            System.out.print("Enter the city: ");
+	            String city = scanner.nextLine();
+	            System.out.print("Enter the state: ");
+	            String state = scanner.nextLine();
+	            System.out.print("Enter the zip code: ");
+	            String zipCode = scanner.nextLine();
+	            System.out.print("Enter the phone number: ");
+	            String phoneNumber = scanner.nextLine();
+	            System.out.print("Enter the email: ");
+	            String email = scanner.nextLine();
+	            contacts.add(new AddressBookMain(firstName,lastName,address,city,state,zipCode,phoneNumber,email));
+	        }
+	        for(AddressBookMain cont: contacts)
+	        {
+	            cont.PrintAddresBookDetails();
+	        }
 	}
 
 }
